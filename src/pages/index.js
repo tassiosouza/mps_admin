1febe7a37c8aad31add13effea0a23a0498db67c
Tassio Souza <souza.tassio@gmail.com>
2022-08-01 13:55:41 -0700

    add src folder

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,7 @@ import styles from '../styles/Home.module.css'
 import { withAuthenticator, Button, Heading } from '@aws-amplify/ui-react';
 import Amplify from 'aws-amplify';
 import '@aws-amplify/ui-react/styles.css';
-import awsconfig from '../aws-exports';
+import awsconfig from '../../aws-exports';
 
 Amplify.configure(awsconfig);
 
